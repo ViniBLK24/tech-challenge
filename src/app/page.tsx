7a4365faa233ea430fafb708e27e5e15bc9dfc8b
Vinicius Ribeiro <vinii.rib@gmail.com>
2025-05-19 16:53:34 -0300

    style: add logo to header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,18 +5,18 @@ export default function Home() {
     <header className="flex items-center justify-between px-8 py-4 bg-black">
       {/* Logo */}
       <div className="flex items-center">
-        <Image src="/logo.png" alt="Bytebank" width={120} height={32} />
+        <Image src="/logo-green.svg" alt="Bytebank" width={120} height={32} />
       </div>
       {/* Menu */}
       <nav className="flex gap-8">
-        <a href="#" className="text-white hover:text-green-400">Sobre</a>
-        <a href="#" className="text-white hover:text-green-400">Serviços</a>
+        <a href="#" className="text-pattern-green hover:text-white">Sobre</a>
+        <a href="#" className="text-pattern-green hover:text-white">Serviços</a>
       </nav>
       {/* Botões */}
       <div className="flex gap-4">
         <button className="border border-pattern-green text-pattern-green px-4 py-2 rounded hover:bg-pattern-green hover:text-black font-semibold cursor-pointer">
-  Abrir minha conta
-</button>
+          Abrir minha conta
+        </button>
         <button className="border border-pattern-green text-pattern-green px-4 py-2 rounded hover:bg-pattern-green hover:text-black font-semibold cursor-pointer">
           Já tenho conta
         </button>
