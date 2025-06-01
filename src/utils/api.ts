71257b013b31a6258f1070e0a8d4787054eadea2
Rosana Damasceno <rosanaxica7@yahoo.com.br>
2025-06-01 14:31:04 -0300

    feat(edit):add api routes

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -3,15 +3,33 @@ import { Transaction } from "@/types/transactions";
 const API_URL = "/api/transactions";
 
 export async function createTransaction(transaction: Transaction) {
-    const response = await fetch(API_URL, {
-        method: "POST",
-        headers: {"Content-Type": "application/json"},
-        body: JSON.stringify(transaction)
-    });
-    return response.json();
+  const response = await fetch(API_URL, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(transaction),
+  });
+  return response.json();
 }
 
-export async function getTransactions(){
-    const response = await fetch(API_URL);
-    return response.json();
+export async function getTransactions() {
+  const response = await fetch(API_URL);
+  return response.json();
+}
+
+export async function editTransaction(transaction: Transaction) {
+  const response = await fetch(API_URL + "&id=" + transaction.id, {
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(transaction),
+  });
+  return response.json();
+}
+
+export async function deleteTransaction(transaction: Transaction) {
+  const response = await fetch(API_URL + "&id=" + transaction.id, {
+    method: "DELETE",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(transaction),
+  });
+  return response.json();
 }
