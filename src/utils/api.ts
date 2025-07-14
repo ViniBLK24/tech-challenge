581f49b575e008d655b85c40561fdec654f78826
nathaliaif <nathalia.iro.fernandes@gmail.com>
2025-07-13 22:31:08 -0300

    refactor: filter transactions by currentUser

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,23 +1,31 @@
 import { Transaction } from "@/types/transactions";
+import getCurrentUserId from "./getCurrentUserId";
 
 const API_URL = "/api/transactions";
 
 export async function createTransaction(transaction: Transaction) {
-  const response = await fetch(API_URL, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(transaction),
-  });
-  return response.json();
+  try {
+    const userId = getCurrentUserId();
+    const response = await fetch(`${API_URL}?userId=${userId}`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(transaction),
+    });
+    return response.json();
+  } catch {
+    
+  }
 }
 
 export async function getTransactions() {
-  const response = await fetch(API_URL);
+  const userId = getCurrentUserId();
+  const response = await fetch(`${API_URL}?userId=${userId}`);
   return response.json();
 }
 
 export async function editTransaction(transaction: Transaction) {
-  const response = await fetch(API_URL + "?id=" + transaction.id, {
+  const userId = getCurrentUserId();
+  const response = await fetch(`${API_URL}?userId=${userId}` + "?id=" + transaction.id, {
     method: "PUT",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(transaction),
@@ -26,7 +34,8 @@ export async function editTransaction(transaction: Transaction) {
 }
 
 export async function deleteTransaction(transaction: Transaction) {
-  const response = await fetch(API_URL + "?id=" + transaction.id, {
+  const userId = getCurrentUserId();
+  const response = await fetch(`${API_URL}?userId=${userId}` + "?id=" + transaction.id, {
     method: "DELETE",
     headers: { "Content-Type": "application/json" },
   });
