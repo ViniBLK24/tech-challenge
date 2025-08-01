16fd45c28996c5151780188d2b0e4ba24ab2a441
Gustavo Teixeira <10344032+gustavoteixeirah@users.noreply.github.com>
2025-08-01 20:20:35 -0300

    maybe now

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'standalone',
   images: {
     domains: ["tech-challenge-upload.s3.us-east-2.amazonaws.com"],
   },
